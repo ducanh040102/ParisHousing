4e7f546e3ff25bda4145d68f0c0eebda6ba79f64
ducanh040102 <khoaimon3570@gmail.com>
2022-10-23 13:10:02 +0700

    Paris Housing Model and UI

---
+++ form.py
@@ -0,0 +1,138 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+import main
+
+sqrtM = 0
+numOfRoom = 0
+yard = 0
+pool = 0
+floors = 0
+cityCode = 0
+cityPartRange = 0
+previousOwners = 0
+made = 0
+isNewBuilt = 0
+isStormProtector = 0
+basement = 0
+attic = 0
+garage = 0
+hasStoRoom = 0
+hasGuestRoom = 0
+
+def helloCallBack():
+    sqrtM = en1.get()
+    numOfRoom = en2.get()
+    yard = varY.get()
+    pool = varP.get()
+    floors = en5.get()
+    cityCode = en6.get()
+    cityPartRange = en7.get()
+    previousOwners = en8.get()
+    made = en9.get()
+    isNewBuilt = varB.get()
+    isStormProtector = varS.get()
+    basement = en12.get()
+    attic = en13.get()
+    garage = en14.get()
+    hasStoRoom = varSR.get()
+    hasGuestRoom = en16.get()
+
+    messagebox.showinfo("Prediction",main.predict(sqrtM, numOfRoom, yard, pool, floors, cityCode, cityPartRange, previousOwners, made, isNewBuilt, isStormProtector, basement, attic, garage, hasStoRoom, hasGuestRoom))
+
+base = Tk()
+base.geometry("500x860")
+base.title("Paris Housing")
+
+lb0 = Label(base, text="House Price Prediction", width=20, font=("arial", 24))
+lb0.place(x=80, y=40)
+
+lb1 = Label(base, text="Square Meters", width=20, font=("arial", 12))
+lb1.place(x=20, y=120)
+en1 = Entry(base)
+en1.place(x=200, y=120)
+
+lb2 = Label(base, text="Number Of Rooms", width=20, font=("arial", 12))
+lb2.place(x=19, y=160)
+en2 = Entry(base)
+en2.place(x=200, y=160)
+
+lb3 = Label(base, text="Has Yard", width=20, font=("arial", 12))
+lb3.place(x=5, y=200)
+varY = IntVar()
+Radiobutton(base, text="Yes", padx=5, variable=varY, value=1).place(x=180, y=200)
+Radiobutton(base, text="No", padx=10, variable=varY, value=0).place(x=240, y=200)
+
+lb4 = Label(base, text="Has Pool", width=20, font=("arial", 12))
+lb4.place(x=5, y=240)
+varP = IntVar()
+Radiobutton(base, text="Yes", padx=5, variable=varP, value=1).place(x=180, y=240)
+Radiobutton(base, text="No", padx=10, variable=varP, value=0).place(x=240, y=240)
+
+
+lb5 = Label(base, text="Floors", width=20, font=("arial", 12))
+lb5.place(x=19, y=280)
+en5 = Entry(base)
+en5.place(x=200, y=280)
+
+lb6 = Label(base, text="City Code", width=20, font=("arial", 12))
+lb6.place(x=19, y=320)
+en6 = Entry(base)
+en6.place(x=200, y=320)
+
+lb7 = Label(base, text="City Part Range", width=20, font=("arial", 12))
+lb7.place(x=19, y=360)
+en7 = Entry(base)
+en7.place(x=200, y=360)
+
+lb8 = Label(base, text="Number Of Previous \n Owners", width=20, font=("arial", 12))
+lb8.place(x=19, y=400)
+en8 = Entry(base)
+en8.place(x=200, y=400)
+
+lb9 = Label(base, text="Made", width=20, font=("arial", 12))
+lb9.place(x=19, y=460)
+en9 = Entry(base)
+en9.place(x=200, y=460)
+
+lb10 = Label(base, text="Is New Built", width=20, font=("arial", 12))
+lb10.place(x=5, y=500)
+varB = IntVar()
+Radiobutton(base, text="Yes", padx=5, variable=varB, value=1).place(x=180, y=500)
+Radiobutton(base, text="No", padx=10, variable=varB, value=0).place(x=240, y=500)
+
+lb11 = Label(base, text="Has Storm Protector", width=20, font=("arial", 12))
+lb11.place(x=5, y=540)
+varS = IntVar()
+Radiobutton(base, text="Yes", padx=5, variable=varS, value=1).place(x=180, y=540)
+Radiobutton(base, text="No", padx=10, variable=varS, value=0).place(x=240, y=540)
+
+lb12 = Label(base, text="Basement", width=20, font=("arial", 12))
+lb12.place(x=19, y=580)
+en12 = Entry(base)
+en12.place(x=200, y=580)
+
+lb13 = Label(base, text="Attic", width=20, font=("arial", 12))
+lb13.place(x=19, y=620)
+en13 = Entry(base)
+en13.place(x=200, y=620)
+
+lb14 = Label(base, text="Garage", width=20, font=("arial", 12))
+lb14.place(x=19, y=660)
+en14 = Entry(base)
+en14.place(x=200, y=660)
+
+lb15 = Label(base, text="Has Storage Room", width=20, font=("arial", 12))
+lb15.place(x=5, y=700)
+varSR = IntVar()
+Radiobutton(base, text="Yes", padx=5, variable=varSR, value=1).place(x=180, y=700)
+Radiobutton(base, text="No", padx=10, variable=varSR, value=0).place(x=240, y=700)
+
+lb16 = Label(base, text="Number of Guest \n Room", width=20, font=("arial", 12))
+lb16.place(x=19, y=740)
+en16 = Entry(base)
+en16.place(x=200, y=740)
+
+ttk.Button(base, text="Predict",command=helloCallBack, width=10).place(x=200, y=780)
+
+base.mainloop()
---
+++ main.py
@@ -0,0 +1,69 @@
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.decomposition import PCA
+
+df = np.array(pd.read_csv("./ParisHousing.csv", header=0).values)
+X = df[:, :16]
+Y = df[:, 16]
+
+# print(X)
+# print(Y)
+min = 0.0000000000000001
+max = 0
+
+BestLinear = 0
+BestPCA = 0
+num_pca = 0
+
+X_train, X_valid, Y_train, Y_valid = 0,0,0,0
+
+def predict(sqrt, nOR, hY, hP, flr, cD, cPR, nPO, made, iNB, hSP,basem, attic, gara, hSR, hGR):
+    sample_test = [[sqrt, nOR, hY, hP, flr, cD, cPR, nPO, made, iNB, hSP, basem, attic, gara, hSR, hGR]]
+    print(sample_test);
+    price = BestLinear.predict(BestPCA.transform(sample_test))
+    return price
+
+
+for j in range(1, 8):
+    print("Lan:", j)
+    modelPCA = PCA(n_components=j).fit(X)
+    print(modelPCA)
+
+    X_new = modelPCA.transform(X)  # Dùng để giảm size cho X
+    # giảm size cho X
+
+
+    X_train, X_valid, Y_train, Y_valid = train_test_split(X_new, Y, test_size=0.3, random_state=0)
+
+    modelLinear = LinearRegression().fit(X_train, Y_train)
+    y_pred = modelLinear.predict(X_valid)
+
+    d = 0
+    sum = 0
+
+    for i in range(len(y_pred)):
+        sum += (abs(y_pred[i] - Y_valid[i]))
+
+    mean = sum / len(y_pred)
+
+    for i in range(len(y_pred)):
+        if (abs(y_pred[i] - Y_valid[i]) < mean) :
+            d += 1
+
+    rate = d / len(y_pred)
+
+    print("Classification Accuracy:", rate, "\n")
+
+    if (rate > max):
+        num_pca = j
+        BestPCA = modelPCA
+        max = rate
+        BestLinear = modelLinear
+
+# print("Max:", max, "d=", num_pca)
+#
+# print("Thực tế \t\t Dự đoán" )
+# for i in range (0, 10):
+#     print( Y_valid[i],"\t\t",  y_pred[i])
